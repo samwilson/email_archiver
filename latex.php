5329d18cb67ed22d59b7142d586d4579b57ac2cf
Sam Wilson <sam@samwilson.id.au>
2014-01-01 16:35:54 +0800

    LaTeX new lines.

--- latex.php
+++ latex.php
@@ -47,8 +47,8 @@ foreach ($people as $person_id=>$person_name) {
 echo '\end{document}';
 
 function texEsc($str) {
-    $pat = array('/\\\(\s)/',          '/\\\(\S)/',         '/&/', '/%/', '/\$/',        '/>>/',                       '/_/', '/\^/', '/#/', '/"(\s)/',           '/"(\S)/'         );
-    $rep = array('\textbackslash\ $1', '\textbackslash $1', '\&',  '\%',  '\textdollar ', '\textgreater\textgreater ', '\_',  '\^', '\#',  '\textquotedbl\ $1', '\textquotedbl $1');
+    $pat = array('/\\\(\s)/',          '/\\\(\S)/',         '/&/', '/%/', '/\$/',        '/>>/',                       '/_/', '/\^/', '/#/', '/"(\s)/',           '/"(\S)/', '/[^\n]\n/m');
+    $rep = array('\textbackslash\ $1', '\textbackslash $1', '\&',  '\%',  '\textdollar ', '\textgreater\textgreater ', '\_',  '\^', '\#',  '\textquotedbl\ $1', '\textquotedbl $1', '\\\\\\'."\n");
     return preg_replace($pat, $rep, $str);
 }
 
